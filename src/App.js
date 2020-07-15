e9188fcc2cd54551331112a1b47fb918acdd8243
Mikael Malm <mikaelmalm91@gmail.com>
2020-07-15 18:49:50 +0200

    Rework structure for background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import "./App.scss";
 
-import { SpaceSection } from "sections/Space";
+import {
+  SpaceSection,
+  SkySection,
+  AtmosphereSection,
+  LandSection,
+} from "sections";
 
 function App() {
+  // I modify-the order with flex-direction to reverse how Z-index work :)
+  // Note: Might be not needed after background refactor
   return (
     <div className="App">
       <SpaceSection />
+      <AtmosphereSection />
+      <SkySection />
+      <LandSection />
     </div>
   );
 }
