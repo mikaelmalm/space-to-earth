e455081cfdd665c8ac6799e6db00fb98b664c5e9
Mikael Malm <mikaelmalm91@gmail.com>
2020-07-18 20:18:41 +0200

    Add mega cool animations to the sky and above!

--- src/sections/Atmosphere.js
+++ src/sections/Atmosphere.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect, useRef, useCallback } from "react";
 import styled from "@emotion/styled";
 import { Satelite, Rocket } from "components";
+import { gsap, TimelineMax, Power4, Power1 } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 const StyledAtmosphereSection = styled.div`
   min-height: 40vh;
@@ -9,22 +12,193 @@ const StyledAtmosphereSection = styled.div`
   [data-name="Satelite"] {
     width: 20vmin;
     position: absolute;
-    right: 15vw;
-    top: 5vh;
+    right: 5vw;
+    top: -5vh;
+    transform: rotate(25deg);
+
+    @media (min-width: 920px) {
+      right: 20vw;
+      top: 5vh;
+    }
   }
 
   [data-name="Rocket"] {
-    width: 40vmin;
+    width: 35vmin;
     position: absolute;
-    left: 15vw;
-    bottom: -5vh;
-    transform: rotate(25deg);
+    left: 20vw;
+    bottom: -25vh;
+
+    .start {
+      fill: #09ffe1;
+    }
+
+    .second {
+      fill: #00c9ca;
+
+      path {
+        fill: #00c9ca;
+      }
+    }
+
+    .third {
+      fill: #008fa4;
+
+      path {
+        fill: #008fa4;
+      }
+    }
+
+    .end {
+      fill: #006d84;
+    }
   }
 `;
 
 export const AtmosphereSection = () => {
+  const section = useRef();
+
+  const thrust = useCallback(() => {
+    const thrustTl = new TimelineMax({
+      repeat: -1,
+      ease: Power1.easeInOut,
+    });
+
+    thrustTl
+      .fromTo(
+        [".body.end", ".line", ".dot"],
+        { autoAlpha: 0.2 },
+        {
+          autoAlpha: 1,
+          transformOrigin: "top",
+          duration: 1,
+          yoyo: true,
+          repeat: -1,
+          stagger: 0.1,
+          ease: Power1.easeInOut,
+        }
+      )
+      .to(
+        ".end.line",
+        {
+          y: 5,
+          repeat: -1,
+          stagger: 0.2,
+          autoAlpha: 0,
+          duration: 1,
+          ease: Power1.easeInOut,
+        },
+        0
+      );
+  }, []);
+
+  const rotateSatelite = useCallback(() => {
+    const rotationTl = new TimelineMax({
+      repeat: -1,
+      ease: Power4.easeInOut,
+    });
+
+    rotationTl.timeScale(0.3);
+
+    rotationTl
+      .to(
+        '[data-name="Satelite"] .wing.bottom',
+        2,
+        {
+          scaleY: "0",
+          y: -35,
+          transformOrigin: "top",
+          ease: Power1.easeInOut,
+        },
+        0
+      )
+      // might be a better way to do it in steps
+      .to(
+        '[data-name="Satelite"] .wing.bottom',
+        {
+          rotate: "180deg",
+          transformOrigin: "50%",
+          duration: 0,
+        },
+        1.9
+      )
+      .to(
+        '[data-name="Satelite"] .wing.bottom',
+        2,
+        {
+          scaleY: 1,
+          y: -75,
+          transformOrigin: "top",
+          ease: Power1.easeInOut,
+        },
+        2.2
+      )
+      .to(
+        '[data-name="Satelite"] .wing.top',
+        2,
+        {
+          scaleY: "0",
+          y: 35,
+          transformOrigin: "bottom",
+          ease: Power1.easeInOut,
+        },
+        0
+      )
+      .to(
+        '[data-name="Satelite"] .wing.top',
+        {
+          rotate: "180deg",
+          transformOrigin: "50%",
+          ease: Power1.easeInOut,
+        },
+        1.9
+      )
+      .to(
+        '[data-name="Satelite"] .wing.top',
+        2,
+        {
+          scaleY: 1,
+          y: 75,
+          transformOrigin: "bottom",
+          ease: Power1.easeInOut,
+        },
+        2.2
+      );
+  }, []);
+
+  useEffect(() => {
+    rotateSatelite();
+    thrust();
+
+    const tl = new TimelineMax({
+      scrollTrigger: {
+        id: "AtmosphereSection",
+        trigger: section.current,
+        start: "top center+=100",
+        toggleActions: "play none none",
+      },
+    });
+
+    tl.fromTo(
+      '[data-name="Satelite"]',
+      { autoAlpha: 0 },
+      {
+        duration: 1,
+        autoAlpha: 1,
+        x: -100,
+      }
+    ).fromTo(
+      '[data-name="Rocket"]',
+      { autoAlpha: 0 },
+      {
+        duration: 0.5,
+        autoAlpha: 1,
+        y: -100,
+      }
+    );
+  }, [rotateSatelite, thrust]);
+
   return (
-    <StyledAtmosphereSection>
+    <StyledAtmosphereSection ref={section}>
       <Satelite />
       <Rocket />
     </StyledAtmosphereSection>
--- src/sections/Sky.js
+++ src/sections/Sky.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { gsap, TimelineMax, Power1 } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import styled from "@emotion/styled";
 import {
   LargeCloud,
@@ -7,6 +9,8 @@ import {
   Balloons,
   Parachutes,
 } from "components";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const StyledSkySection = styled.div`
   min-height: 80vh;
@@ -64,6 +68,7 @@ const StyledSkySection = styled.div`
 
   .Cloud {
     flex: 1;
+    opacity: 0;
   }
 
   .Balloons {
@@ -71,14 +76,15 @@ const StyledSkySection = styled.div`
     position: absolute;
     left: 15vmin;
     top: 25vmin;
+    opacity: 0;
   }
 
   .Parachutes {
     position: absolute;
     right: 15vmin;
     bottom: 15vmin;
-
     height: 30vmin;
+    opacity: 0;
   }
 `;
 
@@ -140,8 +146,121 @@ const Sky = () => (
 );
 
 export const SkySection = () => {
+  const section = useRef();
+
+  // clouds
+  useEffect(() => {
+    const tl = new TimelineMax({
+      scrollTrigger: {
+        id: "SkySection",
+        trigger: section.current,
+        start: "top center",
+        toggleActions: "play none none",
+      },
+    });
+
+    tl.to(".background .Cloud", {
+      x: -25,
+      autoAlpha: 1,
+      stagger: 0.01,
+    })
+      .to(
+        ".foreground .Cloud",
+
+        {
+          x: 25,
+          autoAlpha: 1,
+          stagger: 0.01,
+        }
+      )
+      .to(".Cloud", {
+        x: 1,
+        yoyo: true,
+        repeat: -1,
+        duration: 5,
+      });
+  }, []);
+
+  // humans
+  useEffect(() => {
+    const tl = new TimelineMax({
+      scrollTrigger: {
+        id: "HumanSection",
+        trigger: section.current,
+        start: "top center",
+        toggleActions: "play none none",
+      },
+    });
+
+    tl.to(".Balloons", {
+      x: 50,
+      autoAlpha: 1,
+      duration: 1,
+    })
+      .to(
+        ".Parachutes",
+        {
+          x: -50,
+          autoAlpha: 1,
+          duration: 1,
+        },
+        "-=0.5"
+      )
+      .to(
+        ".Balloons .secondary",
+        {
+          y: 8,
+          x: 5,
+          yoyo: true,
+          repeat: -1,
+          ease: Power1.easeInOut,
+          duration: 4,
+        },
+
+        "-=4"
+      )
+      .to(
+        ".Balloons .main",
+        {
+          y: -8,
+          yoyo: true,
+          repeat: -1,
+          ease: Power1.easeInOut,
+          duration: 4,
+        },
+        "-=4"
+      )
+      .to(
+        ".Parachutes .secondary",
+        {
+          y: 10,
+          rotate: "10deg",
+          transformOrigin: "center",
+          yoyo: true,
+          repeat: -1,
+          ease: Power1.easeInOut,
+          duration: 8,
+        },
+
+        "-=4"
+      )
+      .to(
+        ".Parachutes .main",
+        {
+          y: -20,
+          x: 5,
+          rotate: "-7deg",
+          yoyo: true,
+          repeat: -1,
+          ease: Power1.easeInOut,
+          duration: 8,
+        },
+        "-=6"
+      );
+  }, []);
+
   return (
-    <StyledSkySection>
+    <StyledSkySection ref={section}>
       <Sky />
       <Balloons />
       <Parachutes />
